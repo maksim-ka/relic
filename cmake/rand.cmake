4bc0b2d2967cad0b6997623115049f169d2da0b0
maksim-ka <42pema1bif@hft-stuttgart.de>
2020-01-31 15:17:30 +0100

    add flag to help message

--- cmake/rand.cmake
+++ cmake/rand.cmake
@@ -7,6 +7,7 @@ message("   RAND=CALL      Override the generator with a callback.\n")
 
 message(STATUS "Available random number generator seeders (default = UDEV):\n")
 
+message("   SEED=RIOTRND   Use RIOT's random module for seeding. (recommended on RIOT)")
 message("   SEED=WCGR      Use Windows' CryptGenRandom. (recommended)")
 message("   SEED=DEV       Use blocking /dev/random. (recommended)")
 message("   SEED=UDEV      Use non-blocking /dev/urandom. (recommended)")
